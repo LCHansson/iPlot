ceb7c0b4665af2c0375f05d9cd62d2c5c5b10af2
Love Hansson <love.hansson@gmail.com>
2013-10-12 21:24:19 +0200

    Added internal function defaultOptions()

---
+++ R/defaultOptions.R
@@ -0,0 +1,21 @@
+#' Default options for iPlot
+#' 
+#' Add default options to an options list or create a new options list
+#' 
+#' @param options The iPlot options list to pass to the function
+
+defaultOptions <- function(options = list()) {
+  
+  defaultOptionsList <- list(
+    graph = TRUE,
+    table = TRUE
+  )
+  
+  lapply(names(defaultOptionsList), function(i) {
+    if(!is.null(options[[i]])) return()
+    
+    options[[i]] <<- defaultOptionsList[[i]]
+  })
+  
+  return(options)
+}
