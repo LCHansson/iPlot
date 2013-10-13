04f89912830ec496379ad23a7d248c8d051dc245
Thomas Reinholdsson <reinholdsson@gmail.com>
2013-10-13 14:59:44 +0200

    minor fix

--- R/plot.R
+++ R/plot.R
@@ -10,7 +10,7 @@ mini_plot <- function(title, subtitle = "", vec, sel, vec2 = NULL) {
   op <- par(mar = c(2, 0, 1, 0))
   plot(density(vec), xlab = "", ylab = "", lwd=2, axes=F, xaxt="n", yaxt="n", main=" ", cex.axis=0.8)
   if(!is.null(vec2) & length(vec2) >= 2){
-    lines(density(vec2), col="blue")
+    lines(density(vec2), col="blue", lwd=2)
   }
   abline(h = max(density(vec)$y)/2, lty="dashed")
   usr <- par('usr')
