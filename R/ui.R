2257654f0b075686ba4d54d9e2de48d5f4885662
Thomas Reinholdsson <thomas.reinholdsson@pensionsmyndigheten.se>
2013-10-16 10:06:51 +0200

    add ui function

---
+++ R/ui.R
@@ -0,0 +1,57 @@
+ui <- function() {
+  bootstrapPage(
+    includeCSS(system.file("css/custom.css", package="iPlot")),
+    #         includeCSS("inst/css/custom.css"),
+    div(
+      class="row",
+      
+      #### Filters focus area ####
+      div(
+        class="span2",
+        div(
+          class="row",
+          uiOutput("select_filters")
+        ),
+        div(
+          class="row",
+          uiOutput("filters"),
+          tags$hr(),
+          uiOutput("cat_filter")
+        )
+      ),
+      
+      #### Graph focus area ####
+      div(
+        class="span8",
+        div(
+          class="row",
+          uiOutput("select_method")
+        ),
+        div(
+          class="row",
+          plotOutput("graph"),
+          tags$hr()
+        ),
+        
+        #### Table/numeric focus area ####
+        div(
+          class="row",
+          uiOutput("select_analysis")
+        ),
+        div(
+          class="row",
+          uiOutput("analysis")
+        )
+      ),
+      
+      #### Right column focus area ####
+      div(
+        class="span2",
+        div(
+          class="row",
+          div(class="span2",uiOutput("buttons"))
+        )
+      )
+    )
+  )
+}
